7571ef28ae80de9e517051152dfc823064cc129d
kimjinsoo <kimjs@dramancompany.com>
2021-11-28 21:29:39 +0900

    using file system to express fetching data from server side

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import path from "path";
+import fs from "fs/promises";
+
 function HomePage(props) {
   const { products } = props;
 
@@ -21,9 +24,13 @@ function HomePage(props) {
 // 이 부분은 나중에 참고하도록 하자.
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
     },
   };
 }
