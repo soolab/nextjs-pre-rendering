c39162ea67ac8f35423564cf1175dd9bd6a0a2a0
kimjinsoo <kimjs@dramancompany.com>
2021-11-28 23:37:17 +0900

    understanding using getServerSideProps for server-side rendering

---
+++ pages/user-profile.js
@@ -0,0 +1,19 @@
+function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+}
+
+export default UserProfilePage;
+
+// 이걸 쓴다는 것 자체가
+// pre-generated가 아니라는 것임.
+// 이 말은 즉, request 할 때 마다 페이지를 던진다는 말로 이해하면 되겠음.
+// 그럴때는 getStaticProps를 사용하는 것이 아니라 getServerSideProps를 사용하면 된다는 것임.
+// 그 이외의 syntax는 같다고 보면 됨.
+// notFound나 props나 뭐 이런거 형태가 같음.
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      username: "Max",
+    },
+  };
+}
