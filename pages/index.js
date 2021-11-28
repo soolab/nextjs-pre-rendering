51a9d7c50f6c11219335ef139bd74c520db9d896
kimjinsoo <kimjs@dramancompany.com>
2021-11-28 22:56:50 +0900

    understanding notFound and rediect options

--- pages/index.js
+++ pages/index.js
@@ -23,11 +23,28 @@ function HomePage(props) {
 // React 처럼 그떄그떄마다 인터랙션을 해야 할 것 같은데
 // 이 부분은 나중에 참고하도록 하자.
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log("Re generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  // 이런식으로 redirect를 사용할 수 있음.
+  // data 자체가 없으면 말이다.
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  // 이런식으로 notFound를 사용하는 것이
+  // 좋은 것이다. 데이터가 뭔가 패칭이 안되었을떄
+  // 404 페이지로 이동하는 것은 자연스럽기 떄문이다.
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -39,6 +56,10 @@ export async function getStaticProps() {
     // 하지만 아래의 옵션을 걸어주면은 몇 초 마다 해당 props를 업데이트 해주기 떄문에
     // 한계를 극복할 수 있다는 것이다.
     revalidate: 10,
+
+    // 이 옵션을 추가하면은 404페이지가 된다.
+    // 만약 데이터가 패칭되지 않는다면은 이 페이지를 띄우기 위해서이다.
+    // notFound: true
   };
 }
 
