16747db09906eb279fdebf886ed061860b8b9a63
kimjinsoo <kimjs@dramancompany.com>
2021-11-28 23:22:05 +0900

    working with fallback pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -3,7 +3,17 @@ import path from "path";
 import { Fragment } from "react";
 
 function ProductDetailPage(props) {
-  const { products } = props;
+  const { loadedProduct } = props;
+
+  // fallback을 false로 하고
+  // 만약에 p3로 바로 진입하게 되면은
+  // data를 fetching할 시간이 없게 된다.
+  // 그렇기 떄문에 loadedProduct가 null일때는
+  // 이렇게 예외처리를 해줘야 한다는 것이다.
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Fragment>
       <h1>TITLE</h1>
@@ -14,6 +24,9 @@ function ProductDetailPage(props) {
 
 export async function getStaticProps(context) {
   // context 라는 것을 통해서 url의 params 정보를 얻을 수 있다는 것이다.
+  // 여기는 데이터를 어떻게 가져 올 것인가에 대해서 명시를 하는 것 같고.
+  // getStaticPaths는 어떤 것들을 pre generated를 해야하는 것을 명시하는 것 같다.
+  // 이 concept를 잘 이해하도록 해야 한다.
   const { params } = context;
 
   const productId = params.pid;
@@ -47,16 +60,20 @@ export async function getStaticPaths() {
       {
         params: { pid: "p1" },
       },
-      {
-        params: { pid: "p2" },
-      },
-      {
-        params: { pid: "p3" },
-      },
+      //   {
+      //     params: { pid: "p2" },
+      //   },
+      //   {
+      //     params: { pid: "p3" },
+      //   },
     ],
     // 이렇게 fallback을 false로 하면은 pre generated 하게 만드는 것이라고
     // 하는 것 같다.
-    fallback: false,
+    // fallback을 true로 한다면은 p1, p2, p3를 한번에 가져오지 않는다는 것임.
+    // 예를 들면은 p1의 product는 매우 자주 방문되어서 미리 패칭해놓으면은 좋지만,
+    // p2, p3는 그닥 많이 방문하지 않기떄문에 미리 패칭해올 필요가 없다고 생각해보자.
+    // 그럴떄 이 fallback을 true로 사용하고 원하는 것만 미리 패칭하도록 만들면 된다.
+    fallback: true,
   };
 }
 
