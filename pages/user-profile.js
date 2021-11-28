b14e3fb1618085ffb18e4b36e5e3e80a4a99df5d
kimjinsoo <kimjs@dramancompany.com>
2021-11-28 23:40:41 +0900

    get req, res in context

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -11,6 +11,14 @@ export default UserProfilePage;
 // 그 이외의 syntax는 같다고 보면 됨.
 // notFound나 props나 뭐 이런거 형태가 같음.
 export async function getServerSideProps(context) {
+  // req, res를 받을 수 있다는 것이다.
+  // req를 조작하거나 res를 조작할 수 있다는 것이다.
+  // header를 바꾸거나 cookie를 여기서 설정할 수 있을 것이다.
+  const { params, req, res } = context;
+
+  //   console.log(req);
+  //   console.log(res);
+
   return {
     props: {
       username: "Max",
